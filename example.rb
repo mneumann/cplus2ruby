67a85ac5700962aa456aae3e67abcac99821a9f2
Michael Neumann <mneumann@ntecs.de>
2008-03-14 15:35:14 +0100

    Enhance README and example.

--- example.rb
+++ example.rb
@@ -1,21 +1,29 @@
-#
-# Ruby >= 1.9.0 required for this example!
-#
+require 'rubygems'
+$LOAD_PATH.unshift './src'
 require 'cplus2ruby'
 
-class NeuralEntity < Cplus2Ruby_
+class NeuralEntity; cplus2ruby
   property :id
 end
 
 class Neuron < NeuralEntity
-  property :potential,       'float'
-  property :last_spike_time, 'float'
+  property :potential,       :float
+  property :last_spike_time, :float
   property :pre_synapses
 
-  method_c :stimulate, {at: 'float', weight: 'float'}, %{
+  method :stimulate, {:at => :float},{:weight => :float}, %{
     // This is C++ Code
     @potential += at*weight;
+
+    // call a Ruby method
+    log(@potential);
   }
+
+  stub_method :log, {:pot => :float}
+
+  def log(pot)
+    puts "log(#{pot})"
+  end
 
   def initialize
     self.pre_synapses = []
@@ -23,7 +31,11 @@ class Neuron < NeuralEntity
 end
 
 if __FILE__ == $0
-  Cplus2Ruby.compile_and_load('work/neural') 
+  #
+  # Generate C++ code, compile and load shared library. 
+  #
+  Cplus2Ruby.startup('work/neural') 
+
   n = Neuron.new
   n.id = "n1"
   n.potential = 1.0
